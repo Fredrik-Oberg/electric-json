b8966762eee55cb9e4744b2d67665602d9d8cc0d
Fredrik Öberg <fredrik.oberg@ezy.se>
2016-04-07 19:50:45 +0200

    Name change for CSS and html

--- browser/scripts/main.js
+++ browser/scripts/main.js
@@ -3,14 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TreeView from '../views/treeView/treeView.jsx';
 import TextEditor from '../views/textEditor/textEditor.jsx';
-import InspectJson from '../views/inspector/inspector.jsx';
-ReactDOM.render(<div>
-    <TextEditor/>
-    </div>, document.getElementById('rawTextContainer'));
+import Inspector from '../views/inspector/inspector.jsx';
 
 ReactDOM.render(<div>
-    <TreeView />
-    </div>, document.getElementById('parsedJsonContainer'));
+    <TextEditor/>
+    </div>, document.getElementById('textEditor'));
 ReactDOM.render(<div>
-    <InspectJson />
-    </div>, document.getElementById('jsonInspectContainer'));
+    <TreeView/>
+    </div>, document.getElementById('treeView'));
+ReactDOM.render(<div>
+    <Inspector/>
+    </div>, document.getElementById('inspectorContainer'));
---
+++ browser/views/editor/editor.jsx
@@ -0,0 +1,24 @@
+'use babel';
+import React from 'react';
+import JsonStore from '../../scripts/stores/jsonStore';
+import {addRawText} from '../../scripts/actions/jsonActions';
+
+export default class TextEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {rawText: JsonStore.getRawText()};
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e) {
+        addRawText(e.target.value);   
+        //TODO Get the parsedJson    
+    }
+    
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.state.rawText !== nextState.rawText
+    }
+    render() {
+        return <textarea onChange={this.handleChange}
+            defaultValue={this.state.rawText}></textarea>   
+    }
+}
